b12f9260855c021ebd11ec39da99054db5b49ccd
Carl Roth <carvincarl@gmail.com>
2024-02-09 20:17:48 -0600

    updated node 1 gem pickup

--- node_js/1_gem_pickup.js
+++ node_js/1_gem_pickup.js
@@ -15,24 +15,25 @@ This example was written with node 20.10.0.
 
 import fetch from 'node-fetch';
 
-const bot_id = "";
-const bot_token = "";
+// Fill in your bot ID and token.
+const botID = "";
+const botToken = "";
 
 // You should not have to change either of these values.
-const bot_host = "https://engine.beeotee.com";
-const headers = {Authorization: "Bearer " + bot_token,
+const botHost = "https://engine.beeotee.com";
+const headers = {Authorization: "Bearer " + botToken,
                  "Content-Type": "application/json"};
 
 /*****************************************************************************
  * Get your bot info.
  ****************************************************************************/
-const bots_url = bot_host + "/bots/" + bot_id;
-const bots_response = await fetch(bots_url, { headers });
-console.log(bots_response.status + " " + bots_url);
-const bots_data = await bots_response.json();
-console.log(bots_data);
-const grid_id = bots_data.grid;
-console.log("on grid: " + grid_id);
+const botsURL = botHost + "/bots/" + botID;
+const botsResponse = await fetch(botsURL, { headers });
+console.log(botsResponse.status + " " + botsURL);
+const botsData = await botsResponse.json();
+console.log(botsData);
+const gridID = botsData.grid;
+console.log("on grid: " + gridID);
 
 // Wait 3 seconds between all calls.
 await new Promise(r => setTimeout(r, 3000));
@@ -41,13 +42,13 @@ await new Promise(r => setTimeout(r, 3000));
  * Get your grid info.
  * This includes the bot location.
  ****************************************************************************/
-const grids_url = bot_host + "/grids/" + grid_id;
-const grids_response = await fetch(grids_url, { headers });
-console.log(grids_response.status + " " + grids_url);
-const grids_data = await grids_response.json();
-console.log(grids_data);
+const gridsURL = botHost + "/grids/" + gridID;
+const gridsResponse = await fetch(gridsURL, { headers });
+console.log(gridsResponse.status + " " + gridsURL);
+const gridsData = await gridsResponse.json();
+console.log(gridsData);
 // We need the gem id. There should only be 1 gem, so it is easy to find.
-const gem_id = grids_data.items[0].id;
+const gemID = gridsData.items[0].id;
 
 // Wait 3 seconds between all calls.
 await new Promise(r => setTimeout(r, 3000));
@@ -55,16 +56,16 @@ await new Promise(r => setTimeout(r, 3000));
 /*****************************************************************************
  * Move one space to the left.
  ****************************************************************************/
-const move_location = {x: 0, y: 0};
-const move_url = bots_url + "/location";
-const move_response = await fetch(move_url, {
+const moveLocation = {x: 0, y: 0};
+const moveURL = botsURL + "/location";
+const moveResponse = await fetch(moveURL, {
     method: "put",
-    body: JSON.stringify(move_location),
+    body: JSON.stringify(moveLocation),
     headers,
 });
-console.log(move_response.status + " " + move_url);
-const move_data = await move_response.json();
-console.log(move_data);
+console.log(moveResponse.status + " " + moveURL);
+const moveData = await moveResponse.json();
+console.log(moveData);
 
 // Wait 3 seconds between all calls.
 await new Promise(r => setTimeout(r, 3000));
@@ -72,14 +73,14 @@ await new Promise(r => setTimeout(r, 3000));
 /*****************************************************************************
  * Pick up the gem.
  ****************************************************************************/
-const pickup_url = bots_url + "/items/" + gem_id;
-const pickup_response = await fetch(pickup_url, {
+const pickupURL = botsURL + "/items/" + gemID;
+const pickupResponse = await fetch(pickupURL, {
     method: "put",
     headers,
 });
-console.log(pickup_response.status + " " + pickup_url);
-const pickup_data = await pickup_response.json();
-console.log(pickup_data);
+console.log(pickupResponse.status + " " + pickupURL);
+const pickupData = await pickupResponse.json();
+console.log(pickupData);
 
 console.log("Congratulations!");
 console.log("Refresh the web site to see if you've completed the training session.");
